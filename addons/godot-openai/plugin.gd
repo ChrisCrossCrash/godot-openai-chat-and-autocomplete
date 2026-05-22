19fff6cbc2325fb1d305561b6e7a6e9433aec5e3
Christopher Kumm <chrislkumm@protonmail.com>
2026-05-22 09:30:58 -0500

    Rename `dock` to `ai_panel`

--- addons/godot-openai/plugin.gd
+++ addons/godot-openai/plugin.gd
@@ -1,22 +1,22 @@
 @tool
 extends EditorPlugin
-## Registers the AI assist dock and connects it to the editor's
+## Registers the AI assist ai_panel and connects it to the editor's
 ## screen-change signal.
 
 const SCENE_PATH: String = "res://addons/godot-openai/ai_panel.tscn"
 
-## The instantiated AI panel dock. Held so it can be removed on plugin disable.
-var dock: Node
+## The instantiated AI panel ai_panel. Held so it can be removed on plugin disable.
+var ai_panel: Node
 
 
 func _enter_tree() -> void:
-	if not dock:
-		dock = load(SCENE_PATH).instantiate()
-		add_control_to_dock(EditorPlugin.DOCK_SLOT_RIGHT_UL, dock)
-		main_screen_changed.connect(dock._on_main_screen_changed)
-		dock.editor_interface = EditorInterface
+	if not ai_panel:
+		ai_panel = load(SCENE_PATH).instantiate()
+		add_control_to_dock(EditorPlugin.DOCK_SLOT_RIGHT_UL, ai_panel)
+		main_screen_changed.connect(ai_panel._on_main_screen_changed)
+		ai_panel.editor_interface = EditorInterface
 
 
 func _exit_tree() -> void:
-	remove_control_from_docks(dock)
-	dock.queue_free()
+	remove_control_from_docks(ai_panel)
+	ai_panel.queue_free()
